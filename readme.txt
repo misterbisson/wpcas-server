8326690d1861dc53da473751f4dd6dc228bc72c3
Matt Batchelder <borkweb@gmail.com>
2010-04-14 12:50:44 +0000

    Finishing development on version 1 of this plugin.  Features that now work: - CAS Login - CAS Logout - CAS Validate
    Added the following:
    - wpcas_server_login action hook
    - wpcas_server_auth_value filter
    
    
    
    git-svn-id: http://plugins.svn.wordpress.org/wpcas-server/trunk@228862 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,39 @@
+=== Plugin Name ===
+Contributors: borkweb, misterbisson
+Tags: Auth, authentication, central authentication service, wpcas, wpcas-server, integration, phpCAS, CAS
+Requires at least: 2.8
+Tested up to: 2.9.2
+
+Turns WordPress or WordPress MU into a CAS single sign-on authenticator.
+
+== Description ==
+
+This plugin reserves a collection of URIs that create, validate, and destroy CAS tickets.
+
+* /cas/login :: If user is not authenticated he/she is redirected to the login page.  Otherwise the user is redirected to the service specified as a GET variable in the URL - or if service is not provided, the user is redirected to the WordPress instance's home.
+* /cas/logout :: The user's session is destroyed, user is logged out of the WordPress instance, and redirected to $_GET['service'] (or the blog home if service isn't provided) 
+* /cas/proxyValidate and /cas/validate :: The CAS ticket must be passed as a GET parameter in the URL when calling /cas/validate.  The ticket is validated and XML is output with either cas:authenticationSuccess or cas:authenticationFailure
+
+An action hook (wpcas_server_login) is provided during /cas/login to allow for the execution of additional login-specific code (e.g. adding extra session variables, etc).
+
+== Installation ==
+
+1. Upload `wpcas-server` directory to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+
+== Hooks & Filters ==
+
+= wpcas_server_login Hook =
+
+This hook allows for the insertion of code after login has successfully completed and just before the ticket creation.  One common use of this hook is to fill out the $_SESSION variable with site/user specific information.
+
+= wpcas_server_auth_value Filter =
+
+This filter (executed in a successful ticket validation in /cas/validate) is used to override the user identifier returned in the cas:authenticationSuccess XML response.  By default, the value returned is the $user_ID of the authenticated user.  Using this filter, that value can be altered to whatever suits your implementation.
+
+== Changelog ==
+
+= 1.0 =
+* Initial release
--- wpcas-server.php
+++ wpcas-server.php
@@ -1,15 +1,15 @@
 <?php
 /*
-Plugin Name: wpCAS Server
-Version: 0.01
-Plugin URI: http://maisonbisson.com/projects/wordpress-cas-server
-Description: Turns WordPress or WordPress MU into a <a href="http://en.wikipedia.org/wiki/Central_Authentication_Service">CAS</a> single sign-on authenticator.
-Author: Casey Bisson
-Author URI: http://maisonbisson.com/
+Plugin Name: WordPress CAS Server
+Version: 0.05
+Plugin URI: http://borkweb.com/projects/wordpress-cas-server
+Description: Turns WordPress or WordPress MU into a <a href="http://en.wikipedia.org/wiki/Central_Authentication_Service">CAS</a> single sign-on authenticator.  Based on the original, partially completed code by Casey Bisson (http://maisonbisson.com).
+Author: Matthew Batchelder
+Author URI: http://borkweb.com/
 */
 
 /* 
- Copyright (C) 2009 Casey Bisson
+ Copyright (C) 2010 Matthew Batchelder
 
  This program is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
@@ -26,64 +26,138 @@ Author URI: http://maisonbisson.com/
  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA	 02111-1307	 USA 
 */
 
-class wpCAS_server {
+class wpCAS_server 
+{
+	/**
+	 * creates an auth ticket number
+	 *
+	 * @param $type \b type of ticket (ST, TGC, PGT, PGTIOU, PT)
+	 */
+	public function create_ticket($user_id, $type = 'ST')
+	{
+		return $type . '-'. urlencode( str_rot13( wp_generate_auth_cookie( $user_id, time() + 15, 'auth' )));
+	}//end create_ticket
 
-	function wpCAS_server(){
-		if ( preg_match( '/^\/cas\//', $_SERVER['REQUEST_URI'] ) && !is_admin() )
-			add_action( 'init', array( &$this, 'init' ));
-	}
+	/**
+	 * fail the cas request
+	 */
+	public function fail()
+	{
+		die('fail');
+	}//end fail
 
-	function init(){
-		$path = parse_url( $_SERVER['REQUEST_URI'], PHP_URL_PATH );		
-		switch( $path ){
-			case '/cas/login' :
-				$this->login();
-			case '/cas/logout' :
-				$this->logout();
-			case '/cas/validate' :
-				$this->validate();
-			default :
-				$this->fail();
-		}
-	}
+	/**
+	 * return script path
+	 */
+	public function get_path()
+	{
+		return parse_url( $_SERVER['REQUEST_URI'], PHP_URL_PATH );		
+	}//end get_path
 
-	function login(){
+	/**
+	 * initialize the cas request and redirect to the appropriate cas function
+	 */
+	public function init()
+	{
+		$path = self::get_path();
+
+		switch( substr($path, strpos($path, '/cas/') ) )
+		{
+			case '/cas/login/':
+			case '/cas/login' : self::login(); break;
+			case '/cas/logout/':
+			case '/cas/logout' : self::logout(); break;
+			case '/cas/proxyValidate/':
+			case '/cas/proxyValidate' :
+			case '/cas/validate/':
+			case '/cas/validate' : self::validate(); break;
+			default : self::fail(); break;
+		}//end switch
+	}//end init
+
+	/**
+	 * authenticate, create CAS ticket, and pass back to service
+	 */
+	public function login()
+	{
+		global $userdata, $user_ID;
+
 		if( !is_user_logged_in() )
 			die( auth_redirect() );
 
-		$ticket = 'ticket=ST-'. urlencode( str_rot13( wp_generate_auth_cookie( 1, time() + 15, 'auth' )));
+		get_currentuserinfo();
+
+		do_action('wpcas_server_login');
+
+		$ticket = 'ticket=' . self::create_ticket($user_ID);
 		if( isset( $_GET['service'] ) && $service = sanitize_url( $_GET['service'] ))
-			die( wp_redirect( strpos( $service . '?' ) ? $service .'&'. $ticket : $service .'?'. $ticket ));
+		{
+			die( wp_redirect( $service . (strpos( $service . '?' ) ? '&' : '?'). $ticket ) );
+		}//end if
 
 		die( wp_redirect( get_option( 'home' )));
-	}
+	}//end login
 
-	function logout(){
+	/**
+	 * destroy session and redirect
+	 */
+	public function logout()
+	{
+		self::session_start();
+
+		session_unset();
+		session_destroy();
+
 		wp_logout();
-		die( wp_redirect( get_option( 'home' )));
-	}
 
-	function validate(){
-/*
-/validate
-	?service
-	&ticket
-*/
+		die( wp_redirect( $_GET['service'] ? $_GET['service'] : get_option('home') ) );
+	}//end logout
 
-		if( isset( $_GET['ticket'] ) && wp_validate_auth_cookie( str_rot13( substr( $_GET['ticket'], 3 )), 'auth' )){
-			echo "yes\rusername\r";
-		}else{
-			echo "no\r\r";
-		}
+	/**
+	 * initialize the session
+	 */
+	public function session_start()
+	{
+		if($_COOKIE['PHPSESSID']) 
+		{
+			session_start($_COOKIE['PHPSESSID']);
+		}//end if
+		else 
+		{
+			session_start();
+		}//end else
+	}//end session_start
 
-		die();
-	}
+	/**
+	 * validate a given ticket
+	 */
+	public function validate()
+	{
+		self::session_start();
 
-	function fail(){
-		echo 'fail';
-		die();
-	}
+		$path = self::get_path();
 
+		$ticket = substr( $_GET['ticket'], 3 );
+		$decrypted_ticket = str_rot13($ticket);
 
-}
-$wpcas_server = & new wpCAS_server;
+		$response = '<cas:serviceResponse xmlns:cas="'.get_bloginfo('url').'/cas">'."\n";
+		if( isset( $_GET['ticket'] ) && $user_id = wp_validate_auth_cookie( $decrypted_ticket, 'auth' ))
+		{
+			$auth_value = apply_filters('wpcas_server_auth_value', $user_id);
+
+			$response .= '  <cas:authenticationSuccess>'."\n";
+			$response .= '    <cas:user>'.$auth_value.'</cas:user>'."\n";
+			$response .= '  </cas:authenticationSuccess>'."\n";
+		}//end if
+		else
+		{
+			$response .= '  <cas:authenticationFailure code="">no</cas:authenticationFailure>'."\n";
+		}//end else
+		$response .= '</cas:serviceResponse>';
+ 
+		die($response);
+	}//end validate
+}//end class wpCAS_server
+
+if ( strpos( $_SERVER['REQUEST_URI'], '/cas/' ) && !is_admin() )
+	add_action( 'init', array( 'wpCAS_server', 'init' ));
